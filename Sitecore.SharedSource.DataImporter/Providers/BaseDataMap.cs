f2dcd69ba2f90de2c8e98b31d5919f7f3ac590a9
unknown <mike.james@MJAMESNN4V.MEAU.HQ>
2015-08-29 20:31:39 -0500

    Added some additional logging

--- Sitecore.SharedSource.DataImporter/Providers/BaseDataMap.cs
+++ Sitecore.SharedSource.DataImporter/Providers/BaseDataMap.cs
@@ -422,7 +422,7 @@ namespace Sitecore.SharedSource.DataImporter.Providers
         /// <param name="message"></param>
         protected void Log(string errorType, string message)
         {
-            log.AppendFormat("{0} : {1}", errorType, message).AppendLine().AppendLine();
+            log.AppendFormat("DATA_IMPORTER - {0} : {1}", errorType, message).AppendLine().AppendLine();
         }
 
         /// <summary>
@@ -537,7 +537,7 @@ namespace Sitecore.SharedSource.DataImporter.Providers
         public void CreateNewItem(Item parent, object importRow, string newItemName)
         {
             var nItemTemplate = GetNewItemTemplate(importRow);
-
+            var updating = false;
             using (new LanguageSwitcher(ImportToLanguage))
             {
                 //get the parent in the specific language
@@ -597,6 +597,7 @@ namespace Sitecore.SharedSource.DataImporter.Providers
                 }
                 if (newItem != null)
                 {
+                    updating = true;
                     if (SkipExistingItems)
                         return;
                     if (!Overwrite)
@@ -629,6 +630,8 @@ namespace Sitecore.SharedSource.DataImporter.Providers
 
                     //calls the subclass method to handle custom fields and properties
                     ProcessCustomData(ref newItem, importRow);
+
+                    Log("INFO", string.Format("{0} Material {1}", newItem.DisplayName, updating ? "Updated" : "Added"));
                 }
             }
         }
